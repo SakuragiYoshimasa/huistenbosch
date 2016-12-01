a1963cc0117e006c47682b6b53c9efa9a1a6af90
SakuragiYoshimasa <ysrhsp@outlook.com>
2016-12-01 23:09:13 +0900

    add trump prefab and edit animation component

---
+++ huistenbosch_unity/Assets/Scripts/TrumpAnimation/Trump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+//
+//attached Trump object, which is an empty object has two plane
+//
+public class Trump : MonoBehaviour {
+
+	[SerializeField]
+	private Texture2D image;
+	public Texture2D Image {
+		set{ image = value;}
+		get{ return image;}
+	}
+
+	[SerializeField]
+	private GameObject surface_front;
+	[SerializeField]
+	private GameObject surface_back;
+		
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
