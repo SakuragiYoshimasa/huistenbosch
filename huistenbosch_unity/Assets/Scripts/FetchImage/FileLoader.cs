63b456cc1be12165dfd6fc031765b29c370083f3
SakuragiYoshimasa <ysrhsp@outlook.com>
2016-12-04 01:48:23 +0900

    file loaderを staticにしてまとめてlistを返すように

---
+++ huistenbosch_unity/Assets/Scripts/FetchImage/FileLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileLoader : MonoBehaviour {
+
+	public const string FOLDER_PATH = "/Users/sakuragiyoshimasa/Desktop/twitapi/insta_img/";
+	public const string IMAGE_SUFFIX = ".jpg";
+	public const int IMAGE_NUM = 20; 
+	static string texpath;
+
+	static List<Texture2D> LoadTextures(){
+		List<Texture2D> textures = new List<Texture2D> (0);
+		for(int i = 0; i < IMAGE_NUM; i++){
+			Texture2D tex = new Texture2D(0,0);
+			string texpath = FileLoader.FOLDER_PATH + i.ToString () + FileLoader.IMAGE_SUFFIX;
+			tex.LoadImage(LoadBin(texpath));
+			textures.Add (tex);
+		}
+		return textures;
+	}
+
+	static byte[] LoadBin(string path){
+		FileStream fs = new FileStream(path, FileMode.Open);
+		BinaryReader br = new BinaryReader(fs);
+		byte[] buf = br.ReadBytes( (int)br.BaseStream.Length);
+		br.Close();
+		return buf;
+	}
+}
