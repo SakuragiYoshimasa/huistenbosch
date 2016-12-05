ce25a8ddcb2193084a8deb3c804e3eb0d33156e5
SakuragiYoshimasa <ysrhsp@outlook.com>
2016-12-06 01:10:48 +0900

    add image effects, wind simulation, and trump tower sample4

---
+++ huistenbosch_unity/Assets/Scripts/TrumpAnimation/Wind.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Only for Trump
+public class Wind : MonoBehaviour {
+
+	public float coefficient;   // 空気抵抗係数
+	public Vector3 velocity;    // 風速
+
+	void OnTriggerStay(Collider col) {
+		if ( col.GetComponentInParent<Rigidbody>() == null ) {
+			return;
+		}
+		Debug.Log ("Hit");
+		// 相対速度計算
+		var relativeVelocity = velocity - col.GetComponentInParent<Rigidbody>().velocity;
+
+		// 空気抵抗を与える
+		col.GetComponentInParent<Rigidbody>().AddForce(coefficient * relativeVelocity);
+	}
+}
