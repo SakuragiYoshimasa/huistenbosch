f9bd3d91194e030155cb975c69664d4dada1083b
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2016-12-15 01:59:29 +0900

    経過時間でリアルタイムレンダリングに変更の流れ

--- huistenbosch_unity/Assets/Scripts/Controllers/MovieController.cs
+++ huistenbosch_unity/Assets/Scripts/Controllers/MovieController.cs
@@ -1,29 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class MovieController : MonoBehaviour {
 
-	enum PlayMode {
+	public enum PlayMode {
 		PRE_RENDERED,
 		RLT_RENDERING
 	}
 		
-	public const double WHOLE_TIME_SEC = 300.0;
-	public const double PRE_RENDERED_TIME_SEC = 240.0;
+	public const int PRE_RENDERED_TIME_MILLIS = 240000;
+
+	public PlayMode playmode = PlayMode.PRE_RENDERED;
+
+	private int starttime;
+	private int now;
+	private int elapsedTimeMillis;
+
+	private bool isPlaying = false;
+
+	void Update(){
+		if (!isPlaying) return;
+
+		now = DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + 
+		DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
+
+		elapsedTimeMillis = now - starttime;
+
+		if (elapsedTimeMillis > PRE_RENDERED_TIME_MILLIS) {
+			playmode = PlayMode.RLT_RENDERING;
+			switchToRLTRendering ();
+		}
+	}
 
 
 	public void playMovie(){
-		
+		playmode = PlayMode.PRE_RENDERED;
+		starttime = DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
+		isPlaying = true;
 	}
 
 	public void stopMovie(){
-
+		isPlaying = false;
 	}
 
 	public void pauseMovie(){
-
+		isPlaying = false;
 	}
 
-
+	private void switchToRLTRendering(){
+		
+	}
 }
