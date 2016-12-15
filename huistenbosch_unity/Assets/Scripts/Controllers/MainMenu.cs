c48a6362bd19ff16d12c765ea4cbddb4c06d56e8
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2016-12-15 14:10:22 +0900

    GUIをフレキシブルに

--- huistenbosch_unity/Assets/Scripts/Controllers/MainMenu.cs
+++ huistenbosch_unity/Assets/Scripts/Controllers/MainMenu.cs
@@ -5,13 +5,41 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+	#region ControlledGUI
+	//Start view
+	[SerializeField]
+	private RectTransform rTransform_mainmenuText;
+	[SerializeField]
+	private RectTransform rTransform_loadImageButton;
+	[SerializeField]
+	private RectTransform rTransform_twoSplitButton;
+	[SerializeField]
+	private RectTransform rTransform_threeSplitButton;
+	[SerializeField]
+	private RectTransform rTransform_removeImageButton;
+
+	//Playing view
+	[SerializeField]
+	private RectTransform rTransform_previewText;
+	[SerializeField]
+	private RectTransform rTransform_stopButton;
+	[SerializeField]
+	private RectTransform rTransform_previewCanvas;
+	[SerializeField]
+	private RectTransform rTransform_previewCam1;
+	[SerializeField]
+	private RectTransform rTransform_previewCam2;
+	[SerializeField]
+	private RectTransform rTransform_previewCam3;
+
+	#endregion
+
 	#region ActiveControllObjects
 	[SerializeField]
 	private GameObject[] menuObjects;
 	[SerializeField]
 	private GameObject[] previewObjects;
 	#endregion
-
 
 	#region ScrollViewFields
 	[SerializeField]
@@ -110,8 +138,56 @@ public class MainMenu : MonoBehaviour {
 	#endregion
 
 	void Start(){
+		//ResizeMenuGUIComponent ();
 		switchToMenuMode ();
 	}
+
+	#region ResizeGUIComponent
+	void ResizeMenuGUIComponent(){
+
+		float windowWidth = 1920f;
+		float windowHeight = 1080f;
+
+		float offset = 0;
+		float block = windowHeight / 30.0f;
+
+		float buttonSize = windowWidth / 5.0f;
+
+		float height;
+
+		//mainmenuText
+		height = block * 3.0f;
+		rTransform_mainmenuText.sizeDelta = new Vector2(buttonSize,height);
+		rTransform_mainmenuText.localPosition = new Vector2 (0, -offset + windowHeight / 2.0f);
+		offset += height + block; //4
+
+		//loadImageButton
+		height = block * 2.0f;
+		rTransform_loadImageButton.sizeDelta = new Vector2(buttonSize, height);
+		rTransform_loadImageButton.localPosition = new Vector2 (0, -offset + windowHeight / 2.0f);
+		offset += height + block * 0.5f; //6.5
+
+		//twoSplitButtom
+		rTransform_twoSplitButton.sizeDelta = new Vector2(buttonSize, height);
+		rTransform_twoSplitButton.localPosition = new Vector2 (0, -offset + windowHeight / 2.0f);
+		offset += height + block * 0.5f; //9
+
+		//threeSplitButton
+		rTransform_threeSplitButton.sizeDelta = new Vector2(buttonSize, height);
+		rTransform_threeSplitButton.localPosition = new Vector2 (0, -offset + windowHeight / 2.0f);
+		offset += height + block * 0.5f; //11.5
+
+		//removeImageButton
+		rTransform_removeImageButton.sizeDelta = new Vector2(buttonSize, height);
+		rTransform_removeImageButton.localPosition = new Vector2 (0, -offset + windowHeight / 2.0f);
+		offset += height + block * 0.5f; //14
+	}
+
+	void ResizePrevireGUIComponent(){
+		
+	}
+
+	#endregion
 
 	#region SwitchGUIMode
 	public void switchToMenuMode(){
@@ -132,6 +208,5 @@ public class MainMenu : MonoBehaviour {
 			go.SetActive (true);
 		}
 	}
-
 	#endregion
 }
--- huistenbosch_unity/Assets/Scripts/Controllers/MovieController.cs
+++ huistenbosch_unity/Assets/Scripts/Controllers/MovieController.cs
@@ -9,17 +9,38 @@ public class MovieController : MonoBehaviour {
 		PRE_RENDERED,
 		RLT_RENDERING
 	}
+
+	public PlayMode playmode = PlayMode.PRE_RENDERED;
 		
 	public const int PRE_RENDERED_TIME_MILLIS = 240000;
 	public const int WHOLE_PLAYING_TIME_MILLS = 300000;
-
-	public PlayMode playmode = PlayMode.PRE_RENDERED;
 
 	private int starttime;
 	private int now;
 	private int elapsedTimeMillis;
 
 	private bool isPlaying = false;
+
+	[SerializeField]
+	private RenderTexture outputCamSource1;
+	[SerializeField]
+	private RenderTexture outputCamSource2;
+	[SerializeField]
+	private RenderTexture outputCamSource3;
+
+	[SerializeField]
+	private RenderTexture preRenderedMovieSource1;
+	[SerializeField]
+	private RenderTexture preRenderedMovieSource2;
+	[SerializeField]
+	private RenderTexture preRenderedMovieSource3;
+
+	[SerializeField]
+	private RenderTexture rltRenderingMovieSource1;
+	[SerializeField]
+	private RenderTexture rltRenderingMovieSource2;
+	[SerializeField]
+	private RenderTexture rltRenderingMovieSource3;
 
 	void Update(){
 		if (!isPlaying) return;
@@ -38,7 +59,6 @@ public class MovieController : MonoBehaviour {
 		}
 	}
 
-
 	public void playMovie(){
 		playmode = PlayMode.PRE_RENDERED;
 		starttime = DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
@@ -54,6 +74,14 @@ public class MovieController : MonoBehaviour {
 	}
 
 	private void switchToRLTRendering(){
-		
+		outputCamSource1 = rltRenderingMovieSource1;
+		outputCamSource2 = rltRenderingMovieSource2;
+		outputCamSource3 = rltRenderingMovieSource3;
+	}
+
+	private void switchToPreRendered(){
+		outputCamSource1 = preRenderedMovieSource1;
+		outputCamSource2 = preRenderedMovieSource2;
+		outputCamSource3 = preRenderedMovieSource3;
 	}
 }
