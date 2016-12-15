ee289581aad157db930a4d5fddff58f29c205066
SakuragiYoshimasa <ysrhsp@outlook.com>
2016-12-15 22:29:06 +0900

    mac winでパスを分けた

--- huistenbosch_unity/Assets/Scripts/FetchImage/FileLoader.cs
+++ huistenbosch_unity/Assets/Scripts/FetchImage/FileLoader.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FileLoader : MonoBehaviour {
-
-	public const string FOLDER_PATH = "/Users/yoshimasasakuragi/Desktop/twiapi/img/";
-	public const string IMAGE_SUFFIX = ".jpg";
-	public const int IMAGE_NUM = 188; 
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+    public const string FOLDER_PATH = "/Users/sakuragi/Desktop/twiapi/insta_img/";
+    public const int IMAGE_NUM = 15;
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+    public const string FOLDER_PATH = "/Users/yoshimasasakuragi/Desktop/twiapi/img/";
+    public const int IMAGE_NUM = 188; 
+#endif
+    public const string IMAGE_SUFFIX = ".jpg";
+	
 	static string texpath;
 
 	public static List<Texture2D> LoadTextures(){
